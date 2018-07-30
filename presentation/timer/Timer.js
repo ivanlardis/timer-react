668f4b65d5952c7b5f4f4126338a5efbedb7f2a9
i_larin <ivan24031993@yandex.ru>
2018-07-30 18:07:53 +0300

    - mvp

--- presentation/timer/Timer.js
+++ presentation/timer/Timer.js
@@ -1,19 +1,58 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View, Image, TouchableOpacity, Alert} from 'react-native';
-import { Toolbar } from 'react-native-material-ui';
+import {Toolbar} from 'react-native-material-ui';
+
+import {Observable, Subject, ReplaySubject, from, of, range, interval} from 'rxjs';
+import {map, filter, switchMap, take} from 'rxjs/operators';
 
 export default class Timer extends Component {
+
+    state = {
+        value: 0
+    }
+
+    componentWillMount() {
+        // this.subscription = Rx.Observable.timer(0, 1000).timestamp().subscribe(::this.setState);
+
+       timer= new TimerPresenter(this )
+
+    }
+
+    show(a: Int) {
+
+        this.setState({
+            value: a
+        });
+
+    }
+
     render() {
         return (
             <View>
-                <Toolbar
-                    centerElement="Таймер"
-                />
+
+                <Text>{this.state.value}</Text>
             </View>
         );
     }
+
+    // componentWillUnmount() {
+    //     this.subscription.dispose();
+    // }
 }
 
+
 class TimerPresenter {
-    
+    constructor( time:Timer) {
+        this.timer = time;
+        interval(1000)
+            .pipe(take(2))
+            .subscribe(a => {
+
+               this.timer.show(a)
+
+
+            })
+    }
+
+
 }
