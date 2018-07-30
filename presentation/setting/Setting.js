69a64c914f6607478dc6498a3f4fe24bc8600d0b
i_larin <ivan24031993@yandex.ru>
2018-07-30 13:43:19 +0300

    - реализован фрагмент настроек

--- presentation/setting/Setting.js
+++ presentation/setting/Setting.js
@@ -7,7 +7,18 @@ export default class SettingView extends Component {
     render() {
         return (
             <View style={{alignItems: 'center'}}>
-                <SettingItem/>
+                <SettingItem type={PREFS_ENUM.PREPARATION_TIME}/>
+                <View style={{  height: 1,width:1000,backgroundColor:'blue'}}/>
+                <SettingItem type={PREFS_ENUM.CYCLE_COUNT}/>
+                <View style={{  height: 1,width:1000,backgroundColor:'blue'}}/>
+                <SettingItem type={PREFS_ENUM.SET_COUNT}/>
+                <View style={{  height: 1,width:1000,backgroundColor:'blue'}}/>
+                <SettingItem type={PREFS_ENUM.REST_BETWEEN_SETS_COUNT}/>
+                <View style={{  height: 1,width:1000,backgroundColor:'blue'}}/>
+                <SettingItem type={PREFS_ENUM.REST_TIME}/>
+                <View style={{  height: 1,width:1000,backgroundColor:'blue'}}/>
+                <SettingItem type={PREFS_ENUM.WORK_TIME}/>
+
 
             </View>
         );
@@ -17,10 +28,11 @@ export default class SettingView extends Component {
 class SettingItem extends Component {
 
 
-    constructor(type: PREFS_ENUM) {
+    constructor() {
         super();
+
         this.state = {
-            type: type,
+            type: PREFS_ENUM.CYCLE_COUNT,
             value: 0
         };
 
@@ -30,36 +42,48 @@ class SettingItem extends Component {
     componentDidMount() {
 
 
-    }
-
-    updateState1() {
-
-        Alert.alert(
-            'Alert Title ' + value)
+        this.setState({
+            type: this.props.type
+        });
+        this.update();
 
     }
-    onPressButtonPlus() {
 
+    update() {
 
         prefs = new Prefs();
-        prefs.get(PREFS_ENUM.CYCLE_COUNT)
-            .then(value => {
+        prefs.get(this.state.type)
+            .then(val => {
 
-                this._updateState1()
+                this.setState(
+                    {value: val}
+                );
+            });
 
 
+    }
+
+    onPressButtonPlus() {
+
+        prefs = new Prefs();
+        prefs.get(this.state.type)
+            .then(val => {
+
+                prefs.save(this.state.type, val + 1)
+
+                this.update();
             });
     }
 
     onPressButtonMinus() {
 
         prefs = new Prefs();
-        prefs.get(PREFS_ENUM.CYCLE_COUNT)
-            .then(function (value) {
+        prefs.get(this.state.type)
+            .then(value => {
                     if (value > 1) {
-                        prefs.save(PREFS_ENUM.SET_COUNT, value - 1)
+                        prefs.save(this.state.type, value - 1)
 
-
+                        this.update();
                     }
 
                 }
@@ -68,14 +92,14 @@ class SettingItem extends Component {
     }
 
 
-
     render() {
-        let value = this.state.value;
+        let value = this.state.type + " = " + this.state.value;
+        let left = "<";
         return (
-            <View style={{flex: 1, height: 100, flexDirection: 'row'}}>
+            <View style={{  height: 50, flexDirection: 'row'}}>
                 <TouchableOpacity
 
-                    onPress={this.onPressButtonMinus}
+                    onPress={this.onPressButtonMinus.bind(this)}
 
 
                     style={{
@@ -83,19 +107,19 @@ class SettingItem extends Component {
                         flex: 1, height: 50, justifyContent: 'center',
                         alignItems: 'center'
                     }}>
-                    <Text> a </Text>
+                    <Text>{left}</Text>
 
                 </TouchableOpacity>
 
 
                 <View style={{
-                    flex: 3, height: 50, backgroundColor: 'skyblue', justifyContent: 'center',
+                    flex: 3, height: 50, justifyContent: 'center',
                     alignItems: 'center'
                 }}>
                     <Text> {value} </Text>
                 </View>
                 <TouchableOpacity
-                    onPress={this.onPressButtonPlus}
+                    onPress={this.onPressButtonPlus.bind(this)}
                     style={{
                         flex: 1, height: 50, justifyContent: 'center',
                         alignItems: 'center'
