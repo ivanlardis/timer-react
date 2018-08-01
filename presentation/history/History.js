bd397d883d46d54b02da43223f67e0acd03907bf
i_larin <ivan24031993@yandex.ru>
2018-08-01 09:12:35 +0300

    - работа с NW

--- presentation/history/History.js
+++ presentation/history/History.js
@@ -1,24 +1,23 @@
 import React, {Component} from 'react';
-import {Text, View,Alert} from 'react-native';
+import {Text, View, Alert} from 'react-native';
 
 export default class HistoryView extends Component {
 
     componentWillMount() {
         // this.subscription = Rx.Observable.presenter(0, 1000).timestamp().subscribe(::this.setState);
-        console.log("saas")
-        fetch('https://timerble-8665b.firebaseio.com/messages.json')
-            .then((response) => response.json())
-            .then((responseJson) => {
 
-                    Alert.alert(""+   Map.from(responseJson).length )
+        var historyRepository = new HistoryRepository();
 
+        var historyView = new HistoryModel(
+            1,
+            2,
+            3,
+            3,
+            12211212,
+            "тест",
+        );
 
-                // return responseJson.movies;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
+        historyRepository.saveData(historyView)
     }
 
     render() {
@@ -29,8 +28,75 @@ export default class HistoryView extends Component {
                 </Text>
 
 
-
             </View>
         );
     }
 }
+
+
+class HistoryModel {
+    constructor(
+        cycleCount: int,
+        restTime: int,
+        setCount: int,
+        workTime: int,
+        time: In64,
+        name: string) {
+
+        this.cycleCount = cycleCount;
+        this.restTime = restTime;
+        this.setCount = setCount;
+        this.workTime = workTime;
+        this.time = time;
+        this.name = name;
+
+
+    }
+
+}
+
+
+class HistoryRepository {
+
+
+
+    saveData(historyModel:HistoryModel){
+
+        fetch('https://timerble-8665b.firebaseio.com/messages.json', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(historyModel),
+        });
+
+
+
+    }
+
+    getDataNV() {
+        return fetch('https://timerble-8665b.firebaseio.com/messages.json')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                var trainList = [];
+
+                for (let prop in responseJson) {
+                    var historyView = new HistoryModel(
+                        responseJson[prop].cycleCount,
+                        responseJson[prop].restTime,
+                        responseJson[prop].setCount,
+                        responseJson[prop].workTime,
+                        responseJson[prop].time,
+                        responseJson[prop].name,
+                    );
+                    trainList.push(historyView)
+                }
+                return trainList;
+            });
+    }
+}
+
+
+
+
